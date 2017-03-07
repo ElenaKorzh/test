8a38b95b0dc8bbba50c275b5ed7cca8682ae0add
Роман Бабаков <r.babakov@donnu.edu.ua>
2017-03-07 17:35:21 +0200

    Вернулся далеко назад

--- VCS/VCS.cpp
+++ VCS/VCS.cpp
@@ -1,12 +1,14 @@
+// VCS.cpp : Defines the entry point for the console application.
+//
+
 #include "stdafx.h"
-#include "stdio.h"
+
+
 int main()
 {
 	int a, b;
 	int c;
 	a = 10; b = 20;
-	c = a + b;
-	printf("%d\n", c);
-    return 0;
+	return 0;
 }
 
